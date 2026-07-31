f9e7d112d16810b2435c85e7b6b17de401f50ffa
momojes <thingsaboutjleyva@gmail.com>
2026-07-31 13:31:13 -0700

    Added shared ORG workflow

--- lisp/org-config.el
+++ lisp/org-config.el
@@ -2,25 +2,52 @@
 
 (require 'org)
 
-(setq org-directory (expand-file-name "~/org/")
+(defconst my-org-directory
+  (expand-file-name "~/org/")
+  "Main directory containing Org files.")
+
+(setq org-directory my-org-directory
       org-default-notes-file
-      (expand-file-name "inbox.org" org-directory)
+      (expand-file-name "inbox.org" my-org-directory)
+
       org-agenda-files
-      (list org-directory))
+      (list
+       (expand-file-name "inbox.org" my-org-directory)
+       (expand-file-name "agenda.org" my-org-directory)
+       (expand-file-name "ideas.org" my-org-directory)
+       (expand-file-name "someday.org" my-org-directory))
 
-(make-directory org-directory t)
-
-(setq org-log-done 'time
+      org-log-done 'time
       org-startup-indented t
-      org-hide-emphasis-markers t)
+      org-hide-emphasis-markers t
+      org-return-follows-link t)
+
+(make-directory my-org-directory t)
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "NEXT(n)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELLED(c@)")))
 
 (setq org-capture-templates
-      '(("t" "Todo" entry
-         (file "inbox.org")
-         "* TODO %?\n  %U\n")
-        ("n" "Note" entry
-         (file "inbox.org")
-         "* %?\n  %U\n")))
+      `(("t" "Todo" entry
+         (file ,(expand-file-name "inbox.org" my-org-directory))
+         "* TODO %?\n  %U\n"
+         :empty-lines 1)
+
+        ("i" "Idea" entry
+         (file ,(expand-file-name "ideas.org" my-org-directory))
+         "* TODO %?\n  %U\n"
+         :empty-lines 1)
+
+        ("j" "Journal entry" entry
+         (file+datetree
+          ,(expand-file-name "journal/journal.org" my-org-directory))
+         "* %U %?\n"
+         :empty-lines 1)
+
+        ("d" "Draft" entry
+         (file ,(expand-file-name "drafts/drafts.org" my-org-directory))
+         "* TODO %?\n  %U\n"
+         :empty-lines 1)))
 
 (provide 'org-config)
 ;;; org-config.el ends here
